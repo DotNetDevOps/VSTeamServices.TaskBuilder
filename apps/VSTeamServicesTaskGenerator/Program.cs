05fbbd54723e92e0865999f31faa5b19b34e21b8
Poul Kjeldager Sørensen <pks@s-innovations.net>
2016-03-15 20:16:39 +0100

    version  bumbs

--- apps/VSTeamServicesTaskGenerator/Program.cs
+++ apps/VSTeamServicesTaskGenerator/Program.cs
@@ -12,7 +12,7 @@ using SInnovations.VSTeamServices.TasksBuilder.Builder;
 using SInnovations.VSTeamServices.TasksBuilder.ConsoleUtils;
 using SInnovations.VSTeamServices.TasksBuilder.ResourceTypes;
 
-[assembly: AssemblyInformationalVersion("1.0.9")]
+[assembly: AssemblyInformationalVersion("1.0.10")]
 [assembly: AssemblyTitle("VisualStudio TeamServices Task Generator")]
 [assembly: AssemblyDescription("Generate Visual Studio Team Services Tasks using S-Innovations Task Library")]
 [assembly: AssemblyCompany("S-Innovations /v Poul Kjeldager Sørensen")]
