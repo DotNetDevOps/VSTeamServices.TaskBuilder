05b7158dd850f50dafd7523298bd545501c5e226
Poul Kjeldager Sørensen <pks@s-innovations.net>
2018-05-31 14:19:25 +0200

    added build metadata

--- tasks/UpdateNugetPackageVersionsTask/Program.cs
+++ tasks/UpdateNugetPackageVersionsTask/Program.cs
@@ -14,7 +14,7 @@ using System.Xml.Linq;
 [assembly: Guid("D9BAFED4-AB18-4F58-968D-86655B4D2CE9")]
 [assembly: AssemblyTitle("Updating Nuget Packages")]
 [assembly: AssemblyDescription("Updating Nuget Packages")]
-[assembly: AssemblyInformationalVersion("1.0.33")]  //Update to do new release
+[assembly: AssemblyInformationalVersion("1.0.34")]  //Update to do new release
 [assembly: AssemblyConfiguration("Utility")]
 [assembly: AssemblyCompany("S-Innovations v/Poul K. Sørensen")]
 [assembly: AssemblyProduct("UpdateNugetPackageVersionsTask")]
@@ -40,6 +40,9 @@ namespace UpdateNugetPackageVersionsTask
         [Option("PackageFeatureName", Default ="pre")]
         public string PackageFeatureName { get; set; }
 
+        [Option("BuildMetadata")]
+        public string BuildMetadata { get; set; }
+
     }
     class Program
     {
@@ -59,8 +62,11 @@ namespace UpdateNugetPackageVersionsTask
            // var rev = appendversion.Substring(idx + 7).PadLeft(2, '0');
           //  appendversion = string.Format("{0}{1}{2}", appendversion.Substring(0, idx), appendversion.Substring(idx, 7), rev);
             Console.WriteLine(appendversion);
-          //  var nugetpath = string.Format(@"{0}\Agent\Worker\Tools\nuget.exe", Environment.GetEnvironmentVariable("AGENT_HOMEDIRECTORY"));
-
+            //  var nugetpath = string.Format(@"{0}\Agent\Worker\Tools\nuget.exe", Environment.GetEnvironmentVariable("AGENT_HOMEDIRECTORY"));
+            if (!string.IsNullOrEmpty(options.BuildMetadata))
+            {
+                appendversion += "+" + options.BuildMetadata;
+            }
 
             foreach (var path in nugets)
             {
